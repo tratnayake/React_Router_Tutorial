5ba05b2e072ae25bdb07a0b6a90271508839e526
T Ratnayake <thilina@tratnayake.me>
2017-09-17 23:28:51 -0700

    Lecture 138.5

---
+++ src/components/posts_new.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
+
+
+class PostsNew extends Component {
+  renderField(field) {
+    return(
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          type="text" className="form-control"
+          {...field.input}
+        />
+        {field.meta.error}
+      </div>
+    )
+  }
+
+  render() {
+    return(
+      <form>
+        <Field
+          label="Title"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+      </form>
+    )
+  }
+}
+
+function validate(values){
+
+  const errors ={};
+
+  //Validate the inputs from 'values"'
+  if (!values.title){
+    errors.title = "Enter a title!"
+  }
+  if (!values.categories){
+    errors.categories = "Enter some categories!"
+  }
+  if (!values.content){
+    errors.content = "Enter some content please!"
+  }
+
+  return errors;
+
+}
+
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm'
+})(PostsNew);
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+//Piece of code that interacts with history library
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import reducers from './reducers';
+import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </Provider>
+  , document.querySelector('.container'));
